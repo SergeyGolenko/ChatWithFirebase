4c1bb80b41eec6914882de81f7bc9eddd68a6871
SergeyGolenko <1989sergiotay@gmail.com>
2021-02-23 14:07:21 +0200

    create model message

---
+++ ChatWithFirebase/Model/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  ChatWithFirebase
+//
+//  Created by Сергей Голенко on 23.02.2021.
+//
+
+import Foundation
+
+class Message {
+    var messageBody = ""
+    var sender = ""
+}
